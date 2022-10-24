36231842368846aedee35cae4f8b2f6a025e403a
Pozyvnoy <92524889+Pozyvnoy@users.noreply.github.com>
2022-10-25 00:00:38 +0300

    Add files via upload

---
+++ KompGraf-3/KompGraf№3/KompGraf№3/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿
+namespace KompGraf_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Clear = new System.Windows.Forms.Button();
+            this.Show = new System.Windows.Forms.Button();
+            this.Write = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(875, 421);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Clear
+            // 
+            this.Clear.Location = new System.Drawing.Point(651, 439);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(236, 73);
+            this.Clear.TabIndex = 1;
+            this.Clear.Text = "Очистить";
+            this.Clear.UseVisualStyleBackColor = true;
+            this.Clear.Click += new System.EventHandler(this.Clear_Click);
+            // 
+            // Show
+            // 
+            this.Show.Location = new System.Drawing.Point(336, 439);
+            this.Show.Name = "Show";
+            this.Show.Size = new System.Drawing.Size(236, 73);
+            this.Show.TabIndex = 2;
+            this.Show.Text = "Отображение";
+            this.Show.UseVisualStyleBackColor = true;
+            this.Show.Click += new System.EventHandler(this.Show_Click);
+            // 
+            // Write
+            // 
+            this.Write.Location = new System.Drawing.Point(12, 439);
+            this.Write.Name = "Write";
+            this.Write.Size = new System.Drawing.Size(236, 73);
+            this.Write.TabIndex = 3;
+            this.Write.Text = "Запись в файл";
+            this.Write.UseVisualStyleBackColor = true;
+            this.Write.Click += new System.EventHandler(this.Write_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(899, 524);
+            this.Controls.Add(this.Write);
+            this.Controls.Add(this.Show);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button Clear;
+        private System.Windows.Forms.Button Show;
+        private System.Windows.Forms.Button Write;
+    }
+}
+
---
+++ KompGraf-3/KompGraf№3/KompGraf№3/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KompGraf_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        String fullPath = Application.StartupPath.ToString() + "/MyProject.txt";
+
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = null;
+        }
+
+        private void Show_Click(object sender, EventArgs e)
+        {
+            Font font1 = new Font("Corbel", 24, FontStyle.Italic);
+
+            Font font2 = new Font("Imprint MT Shadow", 36, FontStyle.Regular);
+
+            Font font3 = new Font("Arial Black", 26 , FontStyle.Bold);
+
+            Graphics graphics = pictureBox1.CreateGraphics();
+
+            StringFormat sf1 = (StringFormat)StringFormat.GenericTypographic.Clone();
+            StringFormat sf2 = (StringFormat)StringFormat.GenericTypographic.Clone();
+            StringFormat sf3 = (StringFormat)StringFormat.GenericTypographic.Clone();
+
+            sf1.Alignment = StringAlignment.Far;
+            sf1.LineAlignment = StringAlignment.Near;
+            sf1.Trimming = StringTrimming.EllipsisWord;
+
+            sf2.Alignment = StringAlignment.Center;
+            sf2.LineAlignment = StringAlignment.Near;
+            sf2.Trimming = StringTrimming.EllipsisWord;
+            sf2.FormatFlags = StringFormatFlags.DirectionVertical;
+
+
+            sf3.Alignment = StringAlignment.Center;
+            sf3.LineAlignment = StringAlignment.Near;
+            sf3.Trimming = StringTrimming.EllipsisWord;
+
+            using (StreamReader readtext = new StreamReader(fullPath))
+            {
+                for(int i = 0; i < 6; i++)
+                {
+                    graphics.DrawString(readtext.ReadLine(), font1, Brushes.Green, 550, i * 40,  sf1);
+                }
+                for (int i = 0; i < 5; i++)
+                {
+                    graphics.DrawString(readtext.ReadLine(), font2, Brushes.Red, i * 60, 150, sf2);
+                }
+                graphics.DrawString(readtext.ReadLine(), font3, Brushes.Blue, 400, 10, sf3);
+            }
+        }
+
+        private void Write_Click(object sender, EventArgs e)
+        {
+            string str1 = "Hello";
+            string str2 = "How are you";
+            string str3 = "Bye Bye";
+
+            using (StreamWriter writetext = new StreamWriter(fullPath, false))
+            {
+                for(int i = 0; i < 6; i++)
+                {
+                    writetext.WriteLine(str1);
+                }
+                for (int i = 0; i < 5; i++)
+                {
+                    writetext.WriteLine(str2);
+                }
+                writetext.WriteLine(str3);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SetStyle(ControlStyles.ResizeRedraw, true);
+        }
+    }
+}
